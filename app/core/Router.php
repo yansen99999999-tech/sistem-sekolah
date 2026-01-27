3e46f0138a6ab8de1c15d522da193510b902f465
yansen99999999-tech <yansen99999999@gmail.com>
2026-01-27 14:32:14 +0700

    feat: setup router app

--- app/core/Router.php
+++ app/core/Router.php
@@ -6,7 +6,23 @@ class Router
 
     public function run()
     {
-        echo 'Router is running';
+        $method = $_SERVER['REQUEST_METHOD'] ;
+        $uri  = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+
+        if ($method == 'GET' && $uri == '/students'){
+            echo '<h1>Daftar siswa</h1>';
+            echo '<p>Menampilkan daftar siswa</p>';
+            return;
+        }
+
+        if ($method == 'GET' && $uri == '/students/create'){
+            echo '<h1>Tambah siswa</h1>';
+            echo '<p>Menampilkan form tambah siswa</p>';
+            return;
+        }
+
+        http_response_code(404);
+        echo'<h1>404 - Page Not Found</h1>';
     }
 
 }
