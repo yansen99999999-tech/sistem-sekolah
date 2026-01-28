a69960bc633c64ec8b2c191beda0aaebc1d4cc18
yansen99999999-tech <yansen99999999@gmail.com>
2026-01-28 10:17:10 +0700

    wip: implement router app

--- app/core/Router.php
+++ app/core/Router.php
@@ -5,11 +5,27 @@ use App\Controllers\StudentsController;
 
 class Router 
 {
+    private array $routes = [];
 
-    public function run()
+    public function add(string $method, string $uri, string $controller, string $function): void
+    {
+        $this->routes[] = [
+            'method'=>$method
+            'uri'=> $uri  
+            'controller'=> $controller
+            'function' => $function
+        ];
+    }
     {
         $method = $_SERVER['REQUEST_METHOD'] ;
         $uri  = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+
+
+
+
+foreach($this->routes as $route){
+    
+}
 
         if ($method == 'GET' && $uri == '/students'){
            require_once './app /controllers/StudentControllers.php';
--- public/index.php
+++ public/index.php
@@ -4,5 +4,9 @@ require_once './app/core/Router.php' ;
 use App\Core\Router;
 
 $router =  new Router();
+$router->add('GET','/students','StudentControllers','index');
+$router->add('GET','/students/create','StudentControllers','create');
+$router->add('GET','/students/create','StudentControllers','create');
+
 $router->run();
 ?>
