a9ab65dbbe29e88795edfe1c8d370bd1cf65b712
yansen99999999-tech <yansen99999999@gmail.com>
2026-04-08 09:10:42 +0700

    feat:improvement to view mechanism

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -1,26 +1,29 @@
 <?php
 namespace App\Controllers;
+require_once '../app/core/Controller.php';
 
-class StudentController
+use App\Core\Controller;
+
+class StudentController extends Controller
 {
 
     public function index()
     {
-        require_once '../app/views/students/index.php';
+        $this->view('students.index');
     }
 
     public function create()
     {
-        require_once '../app/views/students/create.php';
+        $this->view('students.create');
     }
 
     public function show(string $id)
     {
-        require_once '../app/views/students/show.php';
+        $this->view('students.show');
     }
      public function edit(string $id)
     {
-        require_once '../app/views/students/edit.php';
+        $this->view('students.edit');
     }
 
 }
